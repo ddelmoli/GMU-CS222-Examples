281c89f53ac95f8fec97d6bef178306c63cb4bf1
Dominic Delmolino <ddelmoli@gmail.com>
2022-09-16 20:24:34 -0400

    Fixed merge

--- Week05/fileCopy_ex1.c
+++ Week05/fileCopy_ex1.c
@@ -1,34 +1,21 @@
 #include <stdio.h> 
 int main() {
    FILE *sfp, *dfp;
-<<<<<<< HEAD
    char sfname[32],dfname[32], text[132];
    
    printf("Enter source file name:");    
-=======
-   char sfname[32], dfname[32], text[132];
-
-   printf("Enter source file name:"); 
->>>>>>> 85e89076816e289ea4031c25155124a67c49768e
    scanf("%s",sfname);
    sfp = fopen(sfname,"r");
    if (sfp == NULL) {
       printf("Error opening source file:%s",sfname);
-<<<<<<< HEAD
       return(1); 
    }
    
-=======
-      return 1; 
-   }
-
->>>>>>> 85e89076816e289ea4031c25155124a67c49768e
    printf("Enter destination file name:");    
    scanf("%s",dfname);
    dfp = fopen(dfname,"w");
    if (dfp == NULL) {
       printf("Error opening destination file:%s",dfname);
-<<<<<<< HEAD
       return(1); 
    }
    
@@ -36,15 +23,6 @@ int main() {
       fprintf(dfp, "%s", text);
    
    printf("File: %s has been copied to %s\n\n",sfname,dfname);
-=======
-      return 1; 
-   }
-
-   while(fgets(text, sizeof(text), sfp) != NULL) 
-      fprintf(dfp, "%s", text);
-
-   printf("File: %s had been copied to %s\n\n",sfname,dfname);
->>>>>>> 85e89076816e289ea4031c25155124a67c49768e
    fclose(sfp); 
    fclose(dfp); 
    return 0;
--- Week05/sscanf_ex2.c
+++ Week05/sscanf_ex2.c
@@ -2,36 +2,18 @@
 #include <stdlib.h>
 int main() {
    FILE *fp;
-<<<<<<< HEAD
    char name[25], buff[BUFSIZ];
    // From stdio.h: macro BUFSIZ is guaranteed to be at least 256.    
    float num;
-=======
-   //In stdio.h: macro BUFSIZ is guaranteed to be at least 256.     
-   char name[25], buff[BUFSIZ];
-   float num;
-
->>>>>>> 85e89076816e289ea4031c25155124a67c49768e
    if ((fp=fopen("test.txt","r")) == NULL) {
       printf("test.txt cannot be opened\n"); 
       return 1;
    }
-<<<<<<< HEAD
    while(fgets(buff,sizeof(buff),fp) != NULL) {
       if (sscanf(buff,"%s %f",name,&num) == 2)
          printf("%s %.1f\n",name,num); 
       else printf("Error: line cannot be printed\n"); 
    }
-=======
-
-   while (fgets(buff,sizeof(buff),fp) != NULL) {
-      if (sscanf(buff,"%s %f",name,&num)==2)
-         printf("%s %.1f\n",name,num); 
-      else
-         printf("Error: line cannot be printed\n"); 
-   }
-
->>>>>>> 85e89076816e289ea4031c25155124a67c49768e
    fclose(fp);
    return 0; 
 }
