0209c47acbdddd48e3ac9ab7efae2579f2028548
Dominic Delmolino <ddelmoli@gmail.com>
2022-09-16 20:22:37 -0400

    Merged

--- Week05/fgets1.c
+++ Week05/fgets1.c
@@ -1,7 +1,6 @@
 #include <stdio.h> 
 #include <string.h>
 int main() {
-<<<<<<< HEAD
     char buffer[16], *pos;
 
     printf("Enter string: "); 
@@ -13,14 +12,3 @@ int main() {
 
     return(0); 
 }
-=======
-   char buffer[16], *pos;
-   printf("Enter string: ");
-   fgets(buffer,sizeof(buffer),stdin);
-   printf("Length of %s is %lu \n",buffer,strlen(buffer));
-   if ((pos=strchr(buffer, '\n')) != NULL) 
-      *pos = '\0';
-   printf("Length of %s is now %lu \n",buffer,strlen(buffer));
-   return 0; 
-}
->>>>>>> 85e89076816e289ea4031c25155124a67c49768e
